bd673741ca2cd5dbc53d75c6f01a6d25c07a249b
Liuxinman <46690197+Liuxinman@users.noreply.github.com>
2019-10-25 21:43:21 -0600

    update

--- app/src/main/java/com/example/mooddiary/AddMoodEventActivity.java
+++ app/src/main/java/com/example/mooddiary/AddMoodEventActivity.java
@@ -10,5 +10,7 @@ public class AddMoodEventActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_mood_event);
+
+
     }
 }
