cbbd11fcb9e3a148d80f7587d402f20e9251330d
Peiran Yu <46502584+TaraYu@users.noreply.github.com>
2019-11-07 02:36:09 -0700

    update signup/add ui

--- app/src/main/java/com/example/mooddiary/SignUpActivity.java
+++ app/src/main/java/com/example/mooddiary/SignUpActivity.java
@@ -6,25 +6,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.nfc.Tag;
+import android.net.Network;
+import android.net.NetworkRequest;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -41,6 +39,7 @@ public class SignUpActivity extends AppCompatActivity {
     public DocumentReference FriendsRef;
 
     public static final String TAG = SignUpActivity.class.getSimpleName();
+
 
     /**
      * This creates the view of login
@@ -65,46 +64,52 @@ public class SignUpActivity extends AppCompatActivity {
 
 
     public void initButton() {
-
         confirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
                 boolean valid = true;
                 String usernamePat = "^([a-z0-9A-Z]{3,20})$";
                 if (!Pattern.matches(usernamePat, newUser.getText().toString())) {
-                    newUser.setError("username should more 3 and less than 20 characters with only letters or numbers");
+                    newUser.setError("Username should more 3 and less than 20 characters with only letters or numbers");
                     newUser.setText("");
                     valid = false;
                 }
-                if(valid){
+                if (valid) {
                     userName = newUser.getText().toString();
-                    MoodList moodList = new MoodList();
-                    ArrayList<String> friendsList = new ArrayList<>();
-                    Map<String, Object> friendsData = new HashMap<>();
-                    friendsData.put("FriendsList",friendsList);
-                    MoodRef = db.collection("users").document("users").collection(userName).document("MoodList");
-                    FriendsRef = db.collection("users").document("users").collection(userName).document("FriendsList");
-                    FriendsRef.set(friendsData);
-                    MoodRef.set(moodList)
-                            .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                @Override
-                                public void onSuccess(Void aVoid) {
-                                    Toast.makeText(SignUpActivity.this,"New User signed up",Toast.LENGTH_SHORT).show();
-                                    Log.d(TAG, "DocumentSnapshot successfully written!");
-                                }
-                            })
-                            .addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    Toast.makeText(SignUpActivity.this,"Fail to signed up",Toast.LENGTH_SHORT).show();
-                                    Log.w(TAG,"Error adding document",e);
-                                }
-                            });
-
-                    Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                    startActivity(intent);
+                    db.collection("users").document("users").collection(userName).document("MoodList").get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                        @Override
+                        public void onSuccess(DocumentSnapshot documentSnapshot) {
+                            if (documentSnapshot.exists()) {
+                                newUser.setError("Username already exist");
+                                newUser.setText("");
+                            } else {
+                                MoodList moodList = new MoodList();
+                                ArrayList<String> friendsList = new ArrayList<>();
+                                Map<String, Object> friendsData = new HashMap<>();
+                                friendsData.put("FriendsList", friendsList);
+                                MoodRef = db.collection("users").document("users").collection(userName).document("MoodList");
+                                FriendsRef = db.collection("users").document("users").collection(userName).document("FriendsList");
+                                FriendsRef.set(friendsData);
+                                MoodRef.set(moodList)
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Toast.makeText(SignUpActivity.this, "New User signed up", Toast.LENGTH_SHORT).show();
+                                                Log.d(TAG, "DocumentSnapshot successfully written!");
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Toast.makeText(SignUpActivity.this, "Fail to signed up", Toast.LENGTH_SHORT).show();
+                                                Log.w(TAG, "Error adding document", e);
+                                            }
+                                        });
+                                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                                startActivity(intent);
+                            }
+                        }
+                    });
                 }
             }
         });
@@ -117,6 +122,4 @@ public class SignUpActivity extends AppCompatActivity {
             }
         });
     }
-
-
 }
