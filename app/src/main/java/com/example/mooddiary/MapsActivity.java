7abe8f1917ea9d4226a2d56617fc86cecdbea7ea
Acc33 <56044623+Acc33@users.noreply.github.com>
2019-11-07 19:45:01 -0700

    sydney deleted

--- app/src/main/java/com/example/mooddiary/MapsActivity.java
+++ app/src/main/java/com/example/mooddiary/MapsActivity.java
@@ -92,9 +92,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
+        /*
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        
+         */
         if (map.equals("mymap")){
             getMyMapMoods(myMoods);
             setMyMapMarker();
